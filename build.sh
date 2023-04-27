715bd90efd25ce6680f07cf81c6f03df56f3b520
John Nguyen <chuanna12@gmail.com>
2023-04-27 12:00:32 +0800

    Update build script for more information

--- build.sh
+++ build.sh
@@ -24,22 +24,40 @@ warn() {
     echo -e "\033[1m[WARN] \033[0m" $@ | tee -a ${BUILDLOG}
 }
 
+ask()
+{
+    while [ 1 ]; do
+        read -p "$1 (y/n)?" choice
+        case "$choice" in 
+            y|Y ) 
+                return 0
+                ;;
+            n|N )
+                return 1
+                ;;
+            * ) 
+                echo "invalid";;
+        esac
+    done
+}
+
+
 rm ${BUILDLOG}
 
-# info "\n\
-# ================================================================================\n\
-# Start compiling Software and release bin files for LINS355\n\
-# Info:\n\
-# - 3rd: libserial, googletest\n\
-# - Version: ${VERSION}\n\
-# ================================================================================\n\
-# "
+info "\n\
+================================================================================\n\
+Start compiling Software and release bin files for LINS355\n\
+Info:\n\
+- 3rd: libserial, googletest\n\
+- Version: ${VERSION}\n\
+================================================================================\n\
+"
+
 # ask "Are you sure to continue compiling software packages"
 # if [ ${?} -ne 0 ]; then
 #     exit 4
 # fi
 
-info "Start compiling SW"
 export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib/
 
 mkdir build
@@ -52,5 +70,8 @@ cp unit_test/test_lins355 ../
 cd ..
 # rm -rf build
 
-info "Run SW by the bin file: m2m-serial"
-info "Run unit testing by the bin file: unit_test"
+info "\n\
+================================================================================\n\
+The compiling process is done, please refer to README file for how to test!\n\
+================================================================================\n\
+"
